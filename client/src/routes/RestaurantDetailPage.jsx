43c4196cd14299d5f90e4716e4c5e15408b4ab07
Kevin Zeng <jwzeng2@gmail.com>
2024-05-15 20:40:26 -0500

    Setup the routing for front end.

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-const RestaurantDetailPage = () => {
+// const RestaurantDetailPage = () => {
+//   return (
+//     <div>RestaurantDetailPage</div>
+//   )
+// }
+
+// export default RestaurantDetailPage
+
+import React from "react";
+
+
+
+function RestaurantDetailPage() {
   return (
-    <div>RestaurantDetailPage</div>
-  )
+    <div>
+      <h1>Details page</h1>
+    </div>
+  );
 }
 
-export default RestaurantDetailPage
+export default RestaurantDetailPage;
--- client/src/routes/RestaurantUpdatePage.jsx
+++ client/src/routes/RestaurantUpdatePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// import React from 'react'
 
-const RestaurantUpdatePage = () => {
+// const RestaurantUpdatePage = () => {
+//   return (
+//     <div>RestaurantUpdatePage</div>
+//   )
+// }
+
+// export default RestaurantUpdatePage
+
+import React from "react";
+
+
+
+function RestaurantUpdatePage() {
   return (
-    <div>RestaurantUpdatePage</div>
-  )
+    <div>
+      <h1>Update 1 page</h1>
+    </div>
+  );
 }
 
-export default RestaurantUpdatePage
+export default RestaurantUpdatePage;
