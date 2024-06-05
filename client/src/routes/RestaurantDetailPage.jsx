aed6d250a16a7cb58be9237524db594a8b67e441
Kevin Zeng <jwzeng2@gmail.com>
2024-06-05 15:50:20 -0500

    Finalized the backend for review functionality.

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -13,6 +13,9 @@ import { useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { RestaurantContext } from "../context/RestaurantContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import StarReview from "../components/StarReview";
+import Reviews from "../components/Reviews";
+import AddReview from "../components/AddReview";
 
 function RestaurantDetailPage() {
   const { id } = useParams();
@@ -23,7 +26,7 @@ function RestaurantDetailPage() {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data);
       } catch (err) {
         console.error(err);
       }
@@ -33,7 +36,16 @@ function RestaurantDetailPage() {
 
   return (
     <div>
-      <div>{selectedRestaurant && selectedRestaurant.resname}</div>
+      
+      {selectedRestaurant && (
+        <>
+        <h1 className="text-center display-1" >{selectedRestaurant.restaurant.resname}</h1>
+          <div className="mt-3">
+            <Reviews reviews={selectedRestaurant.reviews} />
+          </div>
+          <AddReview />
+        </>
+      )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -21,7 +21,11 @@ app.use(express.json());
 // Get all restaurants
 app.get("/api/v1/res", async (req, res) => {
   try {
-    const results = await db.query("SELECT * FROM res");
+    // const results = await db.query("SELECT * FROM res");
+
+    const resRatingData = await db.query("select * from res left join (select res_id, count(*), trunc(AVG(rating), 1) as avg_rating from reviews group by res_id) reviews on res.id = reviews.res_id");
+
+    console.log('res rating data',resRatingData);
 
     res.status(200).json({
       status: "success",
@@ -37,18 +41,23 @@ app.get("/api/v1/res", async (req, res) => {
 
 // Get one restaurant
 app.get("/api/v1/res/:id", async (req, res) => {
-  console.log(req.params.id);
+  // console.log(req.params.id);
 
   try {
-    const results = await db.query("SELECT * FROM res WHERE id = $1", [
+    const rest = await db.query("SELECT * FROM res WHERE id = $1", [
       req.params.id,
     ]);
-    console.log(results.rows[0]);
+    // console.log(results.rows[0]);
+
+    const reviews = await db.query("SELECT * FROM reviews WHERE res_id = $1", [
+      req.params.id,
+    ]);
 
     res.status(200).json({
       status: "success",
       data: {
-        restaurant: results.rows[0],
+        restaurant: rest.rows[0],
+        reviews: reviews.rows,
       },
     });
   } catch (err) {
@@ -81,7 +90,7 @@ app.put("/api/v1/res/:id", async (req, res) => {
       "UPDATE res SET resname = $1, locations = $2 WHERE id = $3 returning *",
       [req.body.resname, req.body.locations, req.params.id]
     );
-     
+
     res.status(200).json({
       status: "success",
       data: {
@@ -104,6 +113,24 @@ app.delete("/api/v1/res/:id", async (req, res) => {
 
     res.status(201).json({
       status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// add a review
+app.post("/api/v1/res/:id/addReview", async (req, res) => {
+  try {
+    const newReview = await db.query(
+      "INSERT INTO reviews (res_id, name, review, rating) values ($1, $2, $3, $4) returning *",
+      [req.params.id, req.body.name, req.body.review, req.body.rating]
+    );
+    res.status(201).json({
+      status: "success",
+      data: {
+        review: newReview.rows[0],
+      },
     });
   } catch (err) {
     console.log(err);
